bd27460d39a2a733068fafcbb7aae3e0a939e817
kirill-yasko-axon <kyasko@axondevgroup.com>
2019-12-24 14:41:44 +0200

    123

--- s3.tf
+++ s3.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   acl    = "private"
--- variables.tf
+++ variables.tf
@@ -1,48 +1,14 @@
 # General AWS configuration
 variable "region" {
+default = "eu-central-1"
 }    
-
-# EC2 Configuration
-#--------------------------------------------------------------#
-# SSH
-variable "ssh_key_name" {}
-variable "public_key_path" {}
-variable "private_key_path" {}
-
-# Instance parameters
-variable "instance_name" {}
-variable "amazon_machine_image" {}
-variable "instance_type" {}
-# Storage parameters
-variable "storage_size" {}
-
-# Elastic IP parameters
-variable "eip_name" {}
-
-# Security group parameters
-variable "public_ports" {}
-variable "ssh_allowed_ips" {}
-#--------------------------------------------------------------#
-
 
 # S3 Configuration
 #--------------------------------------------------------------#
 
-variable "bucket_name" {}
-variable "canned_acl" {}
+variable "bucket_name" {
+default = "kirill-yasko-axon-12342"
+}
 
 #--------------------------------------------------------------#
 
-# RDS Configuration
-#--------------------------------------------------------------#
-
-variable "rds_storage_size" {}
-variable "rds_max_storage_size" {}
-variable "rds_engine" {}
-variable "rds_engine_version" {}
-variable "rds_instance_class" {}
-variable "rds_instance_name" {}
-variable "rds_default_username" {}
-variable "rds_default_password" {}
-
-#--------------------------------------------------------------#
