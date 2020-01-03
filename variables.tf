f6f834b956262b1d47afb2ed857498895e33ad29
kirill-yasko-axon <kyasko@axondevgroup.com>
2020-01-03 13:51:22 +0200

    ec2

--- variables.tf
+++ variables.tf
@@ -7,8 +7,59 @@ default = "eu-central-1"
 #--------------------------------------------------------------#
 
 variable "bucket_name" {
-default = "kirill-yasko-axon-12342"
+default = "ziqy-staging"
 }
 
 #--------------------------------------------------------------#
 
+# EC2 Configuration
+#--------------------------------------------------------------#
+# SSH
+
+variable "ssh_key_name" {   
+default = "sandbox-deploy-key"
+}
+ 
+variable "public_key_path" {   
+default = "~/.ssh/id_rsa.pub"
+}
+  
+variable "private_key_path" { 
+default = "~/.ssh/id_rsa"
+}
+ 
+
+# Instance parameters
+
+variable "instance_name" {
+default =  "sandbox"
+}
+
+variable "amazon_machine_image" {
+default =  "ami-02df9ea15c1778c9c"
+}
+
+variable "instance_type" {
+default = "t2.micro"
+}
+
+# Storage parameters
+
+variable "storage_size" {
+default = "20"
+}
+
+# Elastic IP parameters
+variable "eip_name" {
+default = "sandbox_eip"
+}
+
+# Security group parameters
+variable "public_ports" {
+default = [80, 443]
+}
+
+variable "ssh_allowed_ips" { 
+default = ["0.0.0.0/0"]
+}
+#--------------------------------------------------------------#
