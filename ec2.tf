47870e8da4d36a239fdae87f025bb7422f7c53c1
kirill-yasko-axon <kyasko@axondevgroup.com>
2020-01-09 12:34:49 +0200

    add artifacts

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,4 @@
 
-resource "aws_key_pair" "ssh-key" {
-  key_name   = var.ssh_key_name
-  public_key = file(var.public_key_path)
-}
-
 resource "aws_instance" "ec2-instance" {
   ami           = var.amazon_machine_image
   instance_type = var.instance_type
